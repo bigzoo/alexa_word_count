6ae7e8ae950ae11b10b271dbd4f2d0ed43fb4a00
Chris Nyaga <nyagachris75@gmail.com>
2017-06-30 15:00:10 +0300

    After this commit Alexa is able to recognize more special characters in phrase.

--- lib/count.rb
+++ lib/count.rb
@@ -1,7 +1,7 @@
 class String
   define_method(:count) do |word_to_count|
     counter = 0
-    phrase = self.gsub(/[!,.@#{$%^&*()-_+=};:'"]/, ' ')
+    phrase = self.gsub(/[!,.;:'"]/, ' ')
     split = phrase.split(' ')
     split.each() do |word|
       if word==word_to_count
