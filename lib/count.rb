5aa6af9438ee3f552887661192953f83a21d6866
Chris Nyaga <nyagachris75@gmail.com>
2017-07-02 17:03:32 +0300

    After this commit, the count method is updated to count either partial or specific words depending on the partiality stats boolean it recieves.

--- lib/count.rb
+++ lib/count.rb
@@ -1,11 +1,15 @@
 class String
-  define_method(:count) do |word_to_count|
+  define_method(:count) do |params|
+    word_to_count = params[0]
+    partial = params[1]
     counter = 0
     phrase = self.gsub(/[!,.;:'"]/, ' ')
     split = phrase.split(' ')
     split.each() do |word|
       if word==word_to_count
-        counter = counter + 1
+        counter += 1
+      elsif partial && word.include?(word_to_count)
+        counter += 1
       end
     end
     counter
