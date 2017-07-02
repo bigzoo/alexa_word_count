7a9a013fba815f8ef6d8912b108bd56f92921c4b
Chris Nyaga <nyagachris75@gmail.com>
2017-07-02 16:30:25 +0300

    This commit modifies app.rb to pick partiality when present in parameters and pass it to the count form as part of parameters as either a true or false status.

--- app.rb
+++ app.rb
@@ -14,8 +14,15 @@ also_reload('lib/**/*.rb')
  get('/count') do
    phrase = params.fetch('phrase')
    word = params.fetch('word')
-   partial = params.fetch('partial')
+   partial = params.has_key?('partial')
    @word = word
-   @count = phrase.count(word,partial)
+   if partial
+     status = true
+   else
+    status = false
+   end
+   params = Array.new()
+   params.push(word,status)
+   @count = phrase.count(params)
    erb(:count)
  end
